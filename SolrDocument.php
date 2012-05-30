98e4854a732010721da1c22b718360c58c845cf1
Johannes Skov Frandsen <localgod@heaven.dk>
2012-05-30 14:21:21 +0200

    More doc

--- SolrDocument.php
+++ SolrDocument.php
@@ -18,83 +18,313 @@
  */
 class SolrDocument implements ArrayAccess, Iterator, Traversable, Serializable {
 
-	const SORT_DEFAULT = 1 ;
-	const SORT_ASC = 1 ;
-	const SORT_DESC = 2 ;
-	const SORT_FIELD_NAME = 1 ;
-	const SORT_FIELD_VALUE_COUNT = 2 ;
-	const SORT_FIELD_BOOST_VALUE = 4 ;
+	/**
+	 * Default mode for sorting fields within the document.
+	 * @var integer
+	 */
+	const SORT_DEFAULT = 1;
+	/**
+	 * Sorts the fields in ascending order
+	 * @var integer
+	 */
+	const SORT_ASC = 1;
+	/**
+	 * Sorts the fields in descending order
+	 * @var integer
+	 */
+	const SORT_DESC = 2;
+	/**
+	 * Sorts the fields by field name.
+	 * @var integer
+	 */
+	const SORT_FIELD_NAME = 1;
+	/**
+	 * Sorts the fields by number of values in each field.
+	 * @var integer
+	 */
+	const SORT_FIELD_VALUE_COUNT = 2;
+	/**
+	 * Sorts the fields by thier boost values.
+	 * @var integer
+	 */
+	const SORT_FIELD_BOOST_VALUE = 4;
 
-	public function  addField($fieldName,  $fieldValue) {
+	/**
+	 * Adds a field to the document
+	 * 
+	 * This method adds a field to the SolrDocument instance. 
+	 * 
+	 * @param string $fieldName  The name of the field 
+	 * @param string $fieldValue The value of the field. 
+	 * 
+	 * @return boolean TRUE on success or FALSE on failure. 
+	 */
+	public function addField($fieldName,  $fieldValue) {
 	}
-	public function  clear() {
+	/**
+	 * Drops all the fields in the document
+	 * 
+	 * @return boolean TRUE on success or FALSE on failure. 
+	 */
+	public function clear() {
 	}
-	public function  __clone() {
+	/**
+	 * Creates a copy of a SolrDocument object
+	 * 
+	 * Creates a copy of a SolrDocument object. Not to be called directly. 
+	 * 
+	 * @return void
+	 */
+	public function __clone() {
 	}
-	public function  __construct() {
+	/**
+	 * Constructor
+	 * 
+	 * @return void
+	 */
+	public function __construct() {
 	}
-	public function  current() {
+	/**
+	 * Retrieves the current field
+	 * 
+	 * @see Iterator::current()
+	 * @return SolrDocumentField
+	 */
+	public function current() {
 	}
-	public function  deleteField($fieldName) {
+	/**
+	 * Removes a field from the document
+	 *  
+	 * @param string $fieldName Name of the field 
+	 * 
+	 * @return boolean TRUE on success or FALSE on failure. 
+	 */
+	public function deleteField($fieldName) {
 	}
-	public function  __destruct() {
+	/**
+	 * Destructor
+	 * 
+	 * @return void
+	 */
+	public function __destruct() {
 	}
-	public function  fieldExists($fieldName) {
+	/**
+	 * Checks if a field exists in the document
+	 * 
+	 * @param string $fieldName The name of the field. 
+	 * 
+	 * @return boolean TRUE if the field is present and FALSE if it does not. 
+	 */
+	public function fieldExists($fieldName) {
 	}
-	public function   __get($fieldName) {
+	
+	/**
+	 * Acess the field as a property
+	 * 
+	 * @param string $fieldName The name of the field.
+	 * 
+	 * @return SolrDocumentField
+	 */
+	public function __get($fieldName) {
 	}
 
 	/**
 	 * Retrieves a field by name
 	 *
-	 * @param string $fieldName
-	 * @return SolrDocumentField
+	 * @param string $fieldName Name of the field. 
+	 * 
+	 * @return SolrDocumentField|boolean
 	 */
-	public function   getField($fieldName) {
+	public function getField($fieldName) {
 	}
-	public function   getFieldCount() {
+	/**
+	 * Get field count
+	 * 
+	 * Get the number of fields in this document. Multi-value fields are only counted once. 
+	 * 
+	 * @return integer|boolean The number of fields in this document or false
+	 */
+	public function getFieldCount() {
 	}
-	public function   getFieldNames() {
+	/**
+	 * Get field names
+	 * 
+	 * @return array of fields names in the document. 
+	 */
+	public function getFieldNames() {
 	}
 
 	/**
-	 *
-	 * @return SolrInputDocument
+	 * Get a SolrInputDocument equivalent of the object
+	 * 
+	 * This is useful if one wishes to resubmit/update a document retrieved from a query. 
+	 * 
+	 * @return SolrInputDocument|null
 	 */
-	public function  getInputDocument() {
+	public function getInputDocument() {
 	}
-	public function  __isset($fieldName) {
+	/**
+	 * Checks if a field exists 
+	 * 
+	 * @param string $fieldName Name of the field. 
+	 * 
+	 * @return boolean
+	 */
+	public function __isset($fieldName) {
 	}
-	public function   key() {
+	/**
+	 * Retrieves the current key
+	 * 
+	 * @see Iterator::key()
+	 * @return mixed
+	 */
+	public function key() {
 	}
-	public function  merge(SolrDocument &$sourceDoc, $overwrite) {
+	/**
+	 * Merges source to the current SolrDocument
+	 * 
+	 * @param SolrDocument &$sourceDoc The source document. 
+	 * @param unknown_type $overwrite  If this is TRUE then fields with the same name in the destination document will be overwritten.
+	 * 
+	 * @return boolean TRUE on success or FALSE on failure. 
+	 */
+	public function merge(SolrDocument &$sourceDoc, $overwrite) {
 	}
-	public function  next() {
+	/**
+	 * Moves the internal pointer to the next field
+	 * 
+	 * @see Iterator::next()
+	 * @return void
+	 */
+	public function next() {
 	}
-	public function  offsetExists($fieldName) {
+	/**
+	 * Checks if a particular field exists
+	 * 
+	 * This is used when the object is treated as an array. 
+	 * 
+	 * @param string $fieldName The name of the field. 
+	 * 
+	 * @see ArrayAccess::offsetExists()
+	 * @return boolean TRUE on success or FALSE on failure. 
+	 */
+	public function offsetExists($fieldName) {
 	}
-	public function   offsetGet($fieldName) {
+	/**
+	 * Retrieves a field
+	 * 
+	 * This is used to retrieve the field when the object is treated as an array. 
+	 * 
+	 * @param string $fieldName The name of the field.
+	 * 
+	 * @see ArrayAccess::offsetGet()
+	 * @return SolrDocumentField
+	 */
+	public function offsetGet($fieldName) {
 	}
-	public function  offsetSet($fieldName,  $fieldValue) {
+	/**
+	 * Adds a field to the document
+	 * 
+	 * Used when the object is treated as an array to add a field to the document. 
+	 * 
+	 * @param string $fieldName  The name of the field.
+	 * @param string $fieldValue The value for this field.
+	 * 
+	 * @see ArrayAccess::offsetSet()
+	 * @return boolean TRUE on success or FALSE on failure. 
+	 */
+	public function offsetSet($fieldName,  $fieldValue) {
 	}
-	public function  offsetUnset($fieldName) {
+	/**
+	 * Removes a field
+	 * 
+	 * Removes a field from the document. 
+	 * 
+	 * @param string $fieldName The name of the field.
+	 * 
+	 * @see ArrayAccess::offsetUnset()
+	 * @return void
+	 */
+	public function offsetUnset($fieldName) {
 	}
-	public function  reset() {
+	/**
+	 * This is an alias to SolrDocument::clear()
+	 * 
+	 * @return boolean TRUE on success or FALSE on failure. 
+	 */
+	public function reset() {
 	}
-	public function  rewind() {
+	/**
+	 * Resets the internal pointer to the beginning
+	 * 
+	 * @see Iterator::rewind()
+	 * @return void
+	 */
+	public function rewind() {
 	}
-	public function   serialize() {
+	/**
+	 * Used for custom serialization
+	 * 
+	 * @see Serializable::serialize()
+	 * 
+	 * @return string a representing the serialized Solr document. 
+	 */
+	public function serialize() {
 	}
-	public function  __set($fieldName,  $fieldValue) {
+	/**
+	 * Adds another field to the document
+	 * 
+	 * Used to set the fields as new properties.
+	 * 
+	 * @param string $fieldName  The name of the field.
+	 * @param string $fieldValue The value for this field.
+	 * 
+	 * @return boolean TRUE on success or FALSE on failure. 
+	 */
+	public function __set($fieldName,  $fieldValue) {
 	}
-	public function  sort($sortOrderBy, $sortDirection) {
+	/**
+	 * Sorts the fields in the document
+	 * 
+	 * @param unknown_type $sortOrderBy   The sort criteria. 
+	 * @param unknown_type $sortDirection The sort direction.
+	 * 
+	 * @return booelan TRUE on success or FALSE on failure. 
+	 */
+	public function sort($sortOrderBy, $sortDirection) {
 	}
-	public function   toArray() {
+	/**
+	 * Returns an array representation of the document
+	 * 
+	 * @return array representation of the document. 
+	 */
+	public function toArray() {
 	}
-	public function  unserialize($serialized) {
+	/**
+	 * Custom serialization of SolrDocument objects
+	 * 
+	 * @param string $serialized An XML representation of the document. 
+	 * 
+	 * @see Serializable::unserialize()
+	 * @return void
+	 */
+	public function unserialize($serialized) {
 	}
-	public function  __unset($fieldName) {
+	/**
+	 * Removes a field from the document
+	 * 
+	 * @param string $fieldName The name of the field.
+	 * 
+	 * @return boolean TRUE on success or FALSE on failure. 
+	 */
+	public function __unset($fieldName) {
 	}
-	public function  valid() {
+	/**
+	 * Checks if the current position internally is still valid
+	 * 
+	 * @see Iterator::valid()
+	 * @return boolean TRUE on success and FALSE if the current position is no longer valid. 
+	 */
+	public function valid() {
 	}
 }
--- SolrObject.php
+++ SolrObject.php
@@ -18,18 +18,69 @@
  */
 class SolrObject implements ArrayAccess {
 
+	/**
+	 * Creates Solr object
+	 * 
+	 * @return void
+	 */
 	public function __construct() {
 	}
-	public function   __destruct() {
+	/**
+	 * The destructor 
+	 * 
+	 * @return void
+	 */
+	public function __destruct() {
 	}
-	public function   getPropertyNames() {
+	/**
+	 * Get property names
+	 * 
+	 * @return array of all the names of the properties 
+	 */
+	public function getPropertyNames() {
 	}
-	public function   offsetExists($property_name) {
+	/**
+	 * Checks if the property exists
+	 * 
+	 * @param string $property_name The name of the property. 
+	 * 
+	 * @return boolean TRUE on success or FALSE on failure. 
+	 * @see ArrayAccess::offsetExists()
+	 */
+	public function offsetExists($property_name) {
 	}
-	public function   offsetGet($property_name) {
+	/**
+	 * Used to retrieve a property
+	 * 
+	 * @param string $property_name The name of the property.
+	 * 
+	 * @return mixed
+	 * @see ArrayAccess::offsetGet()
+	 */
+	public function offsetGet($property_name) {
 	}
-	public function   offsetSet($property_name,  $property_value) {
+	/**
+	 * Sets the value for a property
+	 * 
+	 * @param string $property_name  The name of the property.
+	 * @param string $property_value The new value. 
+	 * 
+	 * @see ArrayAccess::offsetSet()
+	 * @return void
+	 */
+	public function offsetSet($property_name,  $property_value) {
 	}
-	public function   offsetUnset($property_name) {
+	/**
+	 * Sets the value for the property
+	 * 
+	 * Sets the value for the property. This is used when the object is treated as an array. 
+	 * This object is read-only. This should never be attempted. 
+	 * 
+	 * @param string $property_name The name of the property.
+	 * 
+	 * @see ArrayAccess::offsetUnset()
+	 * @return boolean TRUE on success or FALSE on failure. 
+	 */
+	public function offsetUnset($property_name) {
 	}
 }
